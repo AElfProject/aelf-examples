24a7088db44e8d3a9937e25c834c3b174165f863
Peng Rong <rongpeng@live.com>
2018-12-17 19:21:57 +0800

    Add sidechain example

---
+++ sidechain/settings1.js
@@ -0,0 +1,20 @@
+(function(exports){
+    exports.rpcUrl = "http://path/to/sidechain-1/chain";
+    // exports.rpcUrl = "http://localhost:12341/chain";
+    exports.explorerUrl = "https://explorer.sidechain-1";
+    exports.contractAddress = "ELF_T3oJonaDjRGKsxKqFa1Jqdpjp989noxjYeioNgFjW68pcFVQn"; // Update after deploying contract
+    exports.accounts = [
+        {
+            privkey: "13cd6aab18b99fc5842492c8e1e51a6473bd6034954698cc1cdc8e6f2add15c8",
+            address: "ELF_aaHQLivdEfea51fEDft6q2iJgxUPqFT8W1p3nw3kXn8nDG1t"
+        },
+        {
+            privkey: "64af7a444f58e566a8b007a5a191c0b89b13becec7de4171e6cbe9634ec7dbf0",
+            address: "ELF_bbdAr12rJPKV2fA3LNZXqxa7pp9upaejNJZVKVejG2ZUKykCr"
+        },
+        {
+            privkey: "e3a9bc19bdecd03881740de741d889582d58463b611ed0fbb92b81e6b57ccf20",
+            address: "ELF_ccgwfSiaoF1qEqWtSWtNWkG7HJ2DiFHhanQZGeZGrGSZubWEE"
+        }];
+    // exports.privkey = "13cd6aab18b99fc5842492c8e1e51a6473bd6034954698cc1cdc8e6f2add15c8";
+  }(typeof exports === 'undefined' ? this.settings = {} : exports));
---
+++ sidechain/web1/index.js
@@ -0,0 +1,89 @@
+var address = "";
+var aelf = new Aelf(new Aelf.providers.HttpProvider(settings.rpcUrl));
+aelf.isConnected();
+var wallet = Aelf.wallet.getWalletByPrivateKey(settings.accounts[0].privkey);
+
+var contract = aelf.chain.contractAt(settings.contractAddress, wallet);
+
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+angular.module('aelfdemo', [])
+.controller('MyCtrl', function($scope, $timeout) {
+
+    $scope.explorerUrl = settings.explorerUrl;
+
+    function prepareContractInfo(){
+        $scope.defaultwallet = Aelf.wallet.getWalletByPrivateKey(settings.accounts[0].privkey);
+        $scope.defaultcontract = aelf.chain.contractAt(settings.contractAddress, $scope.defaultwallet);
+        $scope.contractInfo = {};
+        $scope.contractInfo.Symbol = hex2a($scope.defaultcontract.Symbol().return);
+        $scope.contractInfo.TokenName = hex2a($scope.defaultcontract.TokenName().return);
+        $scope.contractInfo.TotalSupply = parseInt("0x"+$scope.defaultcontract.TotalSupply().return);
+    }
+
+    $scope.accounts = settings.accounts;
+    $scope.balances = settings.accounts.map(a=>0);
+    $scope.chain = aelf.chain;
+    $scope.updateBalances = null;
+    $scope.updateBalances = function(){
+        function getBalance(account){
+            var wallet = Aelf.wallet.getWalletByPrivateKey(account.privkey);
+            var contract = aelf.chain.contractAt(settings.contractAddress, wallet);
+            var ret = contract.BalanceOf(account.address).return;
+            return parseInt("0x"+ret);
+        };
+        $scope.accounts.forEach((account, i) => {
+            $scope.accounts[i].bal = getBalance(account);
+        });
+        $timeout(function(){
+            $scope.updateBalances();
+        }, 4000);
+
+    };
+    prepareContractInfo();
+    $scope.updateBalances();
+
+    $scope.getTransactionResult = null;
+    $scope.getTransactionResult = function(txnobj){
+        var res = aelf.chain.getTxResult(txnobj.hash).result;
+        txnobj.status = res.tx_status;
+        if(res.tx_status == "Pending"){
+            $timeout(function(){
+                $scope.getTransactionResult(txnobj);
+            }, 4000);
+        }
+    }
+
+    $scope.getMerklePath = null;
+    $scope.getMerklePath = function(txnobj){
+        var res = aelf.chain.getMerklePath(txnobj.hash);
+        txnobj.merkleproof = res;
+        if(res.hasOwnProperty("error")){
+            $timeout(function(){
+                $scope.getMerklePath(txnobj);
+            }, 4000);
+        }
+    }
+
+    $scope.sentTransactions = [];
+    $scope.sendTransaction = function(){
+        var wallet = Aelf.wallet.getWalletByPrivateKey($scope.accounts[$scope.fromIndex].privkey);
+        var contract = aelf.chain.contractAt(settings.contractAddress, wallet);
+        var hash = contract.Transfer($scope.accounts[$scope.toIndex].address, $scope.transferAmt).hash;
+        var txnobj = {
+            hash: hash,
+            from: $scope.accounts[$scope.fromIndex].address,
+            to: $scope.accounts[$scope.toIndex].address,
+            amount: $scope.transferAmt
+        };
+        $scope.sentTransactions.push(txnobj);
+        $scope.getTransactionResult(txnobj);
+        $scope.getMerklePath(txnobj);
+    };
+});
